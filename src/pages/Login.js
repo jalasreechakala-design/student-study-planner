d4c82565747d89bdb789d988e93189db475731fd
jalasree <jalasreechakala@gmail.com>
2026-06-21 16:16:30 +0530

    Fix backend URLs for production

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,87 +1,87 @@
 import { useState } from "react";
 
 function Login({ setPage, setIsLoggedIn }) {
-    
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
 
-  const loginUser = async () => {
-    if (
-  email.trim() === "" ||
-  password.trim() === ""
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+
+const loginUser = async () => {
+if (
+email.trim() === "" ||
+password.trim() === ""
 ) {
-  alert("Please enter email and password");
-  return;
+alert("Please enter email and password");
+return;
 }
-    const response = await fetch(
-      "https://student-study-planner-qpdr.onrender.com/login",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email,
-          password,
-        }),
-      }
-    );
+const response = await fetch(
+"https://student-study-planner-qpdr.onrender.com/login",
+{
+method: "POST",
+headers: {
+"Content-Type": "application/json",
+},
+body: JSON.stringify({
+email,
+password,
+}),
+}
+);
 
-    const data = await response.json();
+const data = await response.json();  
 
-    if (data.success) {
-  alert("Login Successful");
-  setIsLoggedIn(true);
-  setPage("dashboard");
+if (data.success) {
+
+alert("Login Successful");
+setIsLoggedIn(true);
+setPage("dashboard");
 } else {
-  alert("Invalid Email or Password");
+alert("Invalid Email or Password");
 };
 
-  return (
-    <div>
-      <h1>🔐 Login</h1>
+return (
+<div>
+<h1>🔐 Login</h1>
 
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) =>
-          setEmail(e.target.value)
-        }
-      />
+<input  
+    type="email"  
+    placeholder="Email"  
+    value={email}  
+    onChange={(e) =>  
+      setEmail(e.target.value)  
+    }  
+  />  
 
-      <br />
-      <br />
+  <br />  
+  <br />  
 
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) =>
-          setPassword(e.target.value)
-        }
-      />
+  <input  
+    type="password"  
+    placeholder="Password"  
+    value={password}  
+    onChange={(e) =>  
+      setPassword(e.target.value)  
+    }  
+  />  
 
-      <br />
-      <br />
+  <br />  
+  <br />  
 
-      <button onClick={loginUser}>
-        Login
-      </button>
-      <br />
-<br />
+  <button onClick={loginUser}>  
+    Login  
+  </button>  
+  <br />
 
-<p>
-  Don't have an account?
-</p>
+<br />  <p>  
+  Don't have an account?  
+</p>  <button
+onClick={() => setPage("register")}
 
-<button
-  onClick={() => setPage("register")}
->
-  Register
+> 
+
+Register
 </button>
-    </div>
-  );
+</div>
+);
 }
 
 export default Login;
